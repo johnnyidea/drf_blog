63cb73d09e8b9cf47d210e32bc94b8d221336490
Jonh <jonhash1@yandex.ru>
2022-05-10 00:38:32 +0300

    feature. Added registration

--- api/core/views.py
+++ api/core/views.py
@@ -12,6 +12,7 @@ from taggit.models import Tag
 from rest_framework.views import APIView
 from django.core.mail import send_mail
 from rest_framework import filters
+from .serializers import RegisterSerializer, UserSerializer
 
 
 class FeedBackView(APIView):
@@ -63,4 +64,27 @@ class AsideView(generics.ListAPIView):
     queryset = Post.objects.all().order_by('-id')[:5]
     serializer_class = PostSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class RegisterView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "Пользователь успешно создан",
+        })
+
+class ProfileView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get(self, request, *args,  **kwargs):
+        return Response({
+            "user": UserSerializer(request.user, context=self.get_serializer_context()).data,
+        })
 
